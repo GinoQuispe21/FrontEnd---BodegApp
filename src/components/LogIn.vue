615754604f4f10f9578f8261e88376ae934a7e95
JuanLeycal <leyva.calle1403@gmail.com>
2020-10-21 22:33:20 -0500

    Login created
    Login created but not finished

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,5 +1,40 @@
 <template>
-    <a>prueba</a>
+    <v-card class="mx-auto" max-width="550" outlined>
+
+            <div align="center" class="login">Log In</div>
+
+        <template>
+            <v-row justify="center">
+                <v-col cols="12" sm="10" md="8" lg="6">
+                    <v-card ref="form">
+                        <v-card-text>
+                            <v-text-field
+                                    ref="user"
+                                    v-model="user"
+                                    label="User or Email"
+                                    required outlined>
+                            </v-text-field>
+                            <v-text-field
+                                    ref="password"
+                                    v-model="password"
+                                    label="Password"
+                                    required outlined>
+                            </v-text-field>
+                            <div>
+                                <a>¿Olvidaste la contraseña?</a>
+                            </div>
+                            <v-card-actions>
+                                <v-btn depressed
+                                       color="primary"> Login
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card-text>
+                        <v-divider class="mt-12"></v-divider>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </template>
+    </v-card>
 </template>
 
 <script>
@@ -9,5 +44,13 @@
 </script>
 
 <style scoped>
-
+.login{
+    text-align: center;
+    font-size: 75px;
+    color: white;
+}
+.mx-auto{
+    background: rgb(22,25,29);
+    background: linear-gradient(25deg, rgba(22,25,29,1) 0%, rgba(47,109,228,1) 65%);
+}
 </style>
