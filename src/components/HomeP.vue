9a0aa018479342f56a5f83813d674bf7a006d119
JuanLeycal <leyva.calle1403@gmail.com>
2020-11-26 03:09:38 -0500

    agregado

--- src/components/HomeP.vue
+++ src/components/HomeP.vue
@@ -3,9 +3,8 @@
         <div class="container no-gutters ass">
             <div class="row justify-content-center align-items-center register_body">
                 <div class="col-auto">
-                    <h1>Dedicado a todos nuestros hermanos peruanos y peruanas que </h1>
-                    <h1> lucharon y hasta ahora alzan su voz ante la corrupción por </h1>
-                    <h1>parte del estado.</h1>
+                    <h1>Bienvenido a BodegApp </h1>
+
                 </div>
             </div>
         </div>
